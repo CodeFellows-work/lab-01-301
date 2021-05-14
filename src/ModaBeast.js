5ebf61528a6cbb9815c9cc59e441ed13152b00f4
sunny-lee3 <sunnylee3712@gmail.com>
2021-05-14 10:30:47 -0700

    updated Modal for images

--- src/ModaBeast.js
+++ src/ModaBeast.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button'; 
+import Image from 'react-bootstrap/Image'
 
 
 class ModalBeast extends React.Component {
@@ -18,10 +19,17 @@ class ModalBeast extends React.Component {
             <div>
             <Button style={{marginTop: '10px'}} variant="secondary" onClick={() => {this.handleModal()}}> Open Modal </Button>
               <Modal show={this.state.show} onHide={()=> this.handleModal()}>
-                <Modal.Header closeButton>
+                <Modal.Header>
                 <Modal.Title>{this.props.title}</Modal.Title>
+               
                 </Modal.Header>
-                <Modal.Body>{this.props.description}</Modal.Body>
+          
+                <Modal.Body >
+                  
+                  <Image style={{height: '20rem', width: '29rem'}} src={this.props.src} rounded />
+                  
+                  {this.props.description}
+                </Modal.Body>
                 <Modal.Footer>
                 <Button variant="secondary" onClick={() => this.handleModal()}>
                     Close
